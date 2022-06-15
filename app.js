2007fbefc421125a9c114953346a6b45a0a25a30
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2022-06-15 15:33:30 -0300

    limpeza

--- app.js
+++ app.js
@@ -9,29 +9,12 @@ const app = express();
 app.use(router)
 
 
-/*
-const db = mongoose.connection;
-db.on("error", (error)=> console.error(error));
-db.once("open", ()=>console.log("Conectado ao banco de dados"));
-*/
 
-
-//app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','html');
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-
-
-/*
-app.get("/cadastro", function(req,res){
-    res.sendFile(__dirname +"/views/formulario_usuario.html")
-});
-
-app.get("/login", function(req,res){
-    res.sendFile(__dirname +"/views/tela_login.html")
-});*/
 
 app.listen(8081, function () {
     console.log("Servidor rodando na url http://localhost:8081/ ")
